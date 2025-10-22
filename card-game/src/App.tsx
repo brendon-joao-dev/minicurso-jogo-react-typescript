b03da142ba47c04284ab58adb3349a6384dbfe39
DEV-RUHTRA <atrodrigues2015@gmail.com>
2025-10-22 15:06:22 -0300

    Adicionando outra carta

--- card-game/src/App.tsx
+++ card-game/src/App.tsx
@@ -1,6 +1,8 @@
 // App.tsx
 import { CartaUno } from './components/CartaUno';
 import './App.css';
+import { div } from 'framer-motion/client';
+import { BARALHO_COMPLETO } from './types/gametypes';
 
 function App() {
   // Carta de teste
@@ -10,14 +12,29 @@ function App() {
     valor: '5' as const,
   };
 
+  const cartaTeste2 = {
+    id: "teste2",
+    cor: "azul" as const,
+    valor: "9" as const
+  }
+
   return (
-    <div className="app-uno">
-      <h1>UNO Simplificado - Teste</h1>
-      <div className="area-teste">
-        <CartaUno carta={cartaTeste} />
+    <div>
+      <div className="app-uno">
+        <h1>UNO Simplificado - Teste</h1>
+        <div className="area-teste">
+          <CartaUno carta={cartaTeste} />
+        </div>
+      </div>
+      <div className="app-uno">
+        <div className="area-teste">
+          <CartaUno carta={cartaTeste2} />
+        </div>
       </div>
     </div>
   );
 }
+
+console.log(BARALHO_COMPLETO);
 
 export default App;
